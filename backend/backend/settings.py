98d91b296fe6d26024bf096c38189a3392c28947
Андрей Пашовкин <andre.pashovkin@yandex.ru>
2023-06-03 09:15:05 +0200

    rebuild

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -60,7 +60,6 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
